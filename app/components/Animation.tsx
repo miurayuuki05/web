55c8a975fe0c78763097eb3a092e4392252d9f3a
Faiz Islamic Qowy <67276767+miurayuuki05@users.noreply.github.com>
2023-10-26 11:18:46 +0700

    Animation Error Fixes

--- app/components/Animation.tsx
+++ app/components/Animation.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion"
 import { Inter } from "next/font/google"
 import { useRef } from "react"
 
@@ -7,13 +6,9 @@ const inter = Inter({weight:"700", subsets: ['latin']})
 
 
 export default function Animation(){
-    const cubeConstraints = useRef(null);
     return(
-        <div className={inter.className} ref={cubeConstraints}>
-        <motion.div 
-        drag
-        dragConstraints={cubeConstraints}
-        whileDrag={{scale : 1.5}}
+        <div className={inter.className}>
+        <div
         className="w-96 h-72 self-center flex justify-center">
             <div className="cube">
                 <div className="cube-side"></div>
@@ -23,7 +18,7 @@ export default function Animation(){
                 <div className="cube-side"></div>
                 <div className="cube-side"></div>
             </div>
-        </motion.div>
+        </div>
             <div className="text-3xl text-center">
                 <p>FROM <span className="text-amber-500">IDEAS</span> TO <span className="text-slate-600">CODE</span>,<br/> WE LEAD</p>
             </div>
--- app/components/AnimationPyramid.tsx
+++ app/components/AnimationPyramid.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion"
 import { Inter } from "next/font/google"
 import { useRef } from "react"
 
@@ -7,14 +6,9 @@ const inter = Inter({weight:"700", subsets: ['latin']})
 
 
 export default function AnimationPyramid(){
-    const cubeConstraints = useRef(null);
     return(
-        <div className={inter.className} ref={cubeConstraints}>
-            <motion.div 
-            drag
-            dragConstraints={cubeConstraints}
-            whileDrag={{scale : 1.5}}
-            className="w-96 h-64 self-center flex justify-center">
+        <div className={inter.className}>
+            <div className="w-96 h-64 self-center flex justify-center">
                 <div className="pyramid">
                     <div className="pyramid-side"></div>
                     <div className="pyramid-side"></div>
@@ -22,7 +16,7 @@ export default function AnimationPyramid(){
                     <div className="pyramid-side"></div>
                     <div className="pyramid-side"></div>
                 </div>
-            </motion.div>
+            </div>
             <div className="text-3xl text-center">
                 <p>EXPAND YOUR <span className="text-blue-400">HORIZON</span></p>
                 <p>EXPAND YOUR <span className="text-yellow-200">BUSINESS</span></p>
