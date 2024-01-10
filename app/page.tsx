edfe7f2916ad6df056adb3bb264e176945631c35
Faiz Islamic Qowy <67276767+miurayuuki05@users.noreply.github.com>
2024-01-10 13:07:47 +0700

    fixing project

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,26 @@ import Skills from './components/Skills'
 import Project from './components/Project'
 import Contact from './components/Contact'
 import Svgspacer from './components/Svgspacer'
+import { getProject } from '@/sanity/sanity.query'
+import { Inter } from "next/font/google";
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
-export default function Home() {
+const inter = Inter({weight:"800", subsets: ['latin']})
+
+type ProjectProps  = {
+  project: any;
+}
+
+export const getServerSideProps = (async () => {
+  const project = await getProject();
+  return {
+      props: {
+          project
+      }
+  }
+}) satisfies GetServerSideProps<{ project: ProjectProps }>
+
+export default function Page( { project }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main className="flex flex-col min-h-screen items-center justify-between z-20">
       <Header/>
@@ -26,7 +44,20 @@ export default function Home() {
       <Spacer/>
       <Skills/>
       <Spacer/>
-      <Project/>
+      <div className={inter.className + " text-center pt-20"} id="project">
+            <div className="mb-20">
+                <p className="text-3xl">PROJECTS</p>
+            </div>
+            <div className="flex flex-wrap justify-evenly gap-20">
+                {project && project.map((data : any) => (
+                    <div key={project.title}>
+                        <a href={data.link} target="_blank"><img className="md:w-96 md:h-96 w-64 h-64 object-cover hover:border-4 duration-200 mx-10" src={data.imageUrl} alt="project"/></a>
+                        <p>{data.title}</p>
+                    </div>
+
+                ))}
+            </div>
+        </div>
       <Spacer/>
       <Contact/>
       <Svgspacer/>
